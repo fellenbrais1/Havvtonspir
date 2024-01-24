f5e2fcc36224988317607692e377ed02055cf1c5
fellenbrais1 <106751977+fellenbrais1@users.noreply.github.com>
2024-01-24 13:18:03 +0900

    New uploads

---
+++ searching.py
@@ -0,0 +1,34 @@
+
+# This code shows that we can use a for loop to find a variable in a list
+shopping_list = ['milk', 'pasta', 'eggs', 'spam', 'bread', 'rice']
+
+print(shopping_list)
+item_to_find = input('\nWhat item would you like to find?: ')
+item_to_find = item_to_find.casefold()
+
+# This was replaced by a user function that can specify this instead
+
+# COMMENTED OUT FOR NOW
+# item_to_find = 'spam'
+
+# Here we bind the found_at variable to None to avoid errors later on in the program
+# This could be in a situation where an item is not found in the list for example
+found_at = None
+
+for index in range(len(shopping_list)):
+    if shopping_list[index].casefold() == item_to_find:
+        found_at = index
+        break
+
+if found_at is not None:
+    print('\'{0}\' found at position {1}'.format(item_to_find, found_at))
+else:
+    print('\'{0}\' not found in the list!'.format(item_to_find))
+
+# This is an alternative and much more efficient way of doing this operation in Python
+
+# COMMENTED OUT FOR NOW
+# if item_to_find in shopping_list:
+#     found_at = shopping_list.index(item_to_find)
+# else:
+#     print('\'{0}\' not found in the list!'.format(item_to_find))
---
+++ shopping.py
@@ -0,0 +1,33 @@
+
+# Assigning values to a list, a series of values in quotes separated by commas
+# In Python, we use [] to specify a list
+
+shopping_list = ['milk', 'pasta', 'eggs', 'spam', 'bread', 'rice']
+
+# We can store any type of object in a list in Python
+mix_list = [6, 55.22, True, 'pigs in blankets']
+
+for item in mix_list:
+    print(item, " is: ", type(item))
+
+# One way to exclude an item from the list iteration for loop
+# Most people will do this as the 'continue' key word isn't actually used that much
+print()
+for item in shopping_list:
+    if item != "spam":
+        print("Buy: ", item)
+
+# Another way to exclude an item from the list iteration for loop
+# 'continue' makes all the remaining code in that iteration of the loop to be skipped
+print()
+for item in shopping_list:
+    if item == 'spam':
+        continue
+    print("Buy: ", item)
+
+# 'break' breaks out of the loop completely and stops anything from happening with it
+print()
+for item in shopping_list:
+    if item == 'spam':
+        break
+    print("Buy: ", item)
