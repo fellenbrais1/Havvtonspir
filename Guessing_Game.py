7e122a8c69a9609cfb74543db49ae0b5385f287f
fellenbrais1 <106751977+fellenbrais1@users.noreply.github.com>
2024-01-27 23:28:46 +0900

    Further code improvements
    Removed unnecessary break and continue statements in the code

--- Guessing_Game.py
+++ Guessing_Game.py
@@ -1,16 +1,21 @@
 
 # Modified this code to be more robust and to handle error exceptions concerning the input
 # User can now choose their difficulty from three options
+# Removed unnecessary code (breaks and continues)
 
-# Importing the 'random' module to generate random numbers for the 'answer' variable
-from random import *
+# Importing the 'randint' function from the 'random' module to generate random numbers for the 'answer' variable
+from random import randint
+# from random import *
 
 
 def guesser():
-    answer = randint(1, 9)
+    # The 'highest' variable allows us to specify what the game plays between e.g. between 1 and 'highest'
+    highest = 10
+    answer = randint(1, highest)
     difficulty = ['4', '3', '2']
     guess = 0
     guess_count = 0
+    guess_count_max = 0
 
     print('1. Easy\n2. Medium\n3. Hard')
     ready_1 = True
@@ -21,6 +26,7 @@ def guesser():
             try:
                 guess_count = difficulty[difficulty_input - 1]
                 guess_count = int(guess_count)
+                guess_count_max = guess_count
                 ready_1 = False
             except IndexError:
                 print("Please choose your difficulty by typing the numbers 1 (Easy), 2 (Medium), or 3 (Hard)")
@@ -28,7 +34,8 @@ def guesser():
         except ValueError:
             print("Please choose your difficulty by typing the numbers 1 (Easy), 2 (Medium), or 3 (Hard)")
 
-    print("You must guess a number between 1 and 9, you have {0} guesses \nGood luck!".format(guess_count))
+    print("You must guess a number between 1 and {1}, you have {0} guesses \nGood luck!".format(guess_count, highest))
+    print("You can also enter '0' to quit the game.")
     while guess != answer:
         if guess_count == 0:
             print("I am sorry you have run out of guesses")
@@ -39,27 +46,30 @@ def guesser():
                 print("You have {0} guesses left".format(guess_count))
             else:
                 print("You have {0} guess left".format(guess_count))
-            print("Please guess a number between 1 and 10 ")
+            print("Please guess a number between 1 and {0} ".format(highest))
             try:
                 guess = int(input())
-                if 0 < guess < 10:
+                if guess == 0:
+                    print("See you next time!")
+                    exit()
+                if 0 < guess < highest + 1:
                     if guess == answer:
-                        print("Well done, you got the correct guess!")
-                        break
+                        if guess_count == guess_count_max:
+                            print("Well done, you got the correct guess on your first try!")
+                            print("You win the grand prize! If we had one!")
+                        else:
+                            print("Well done, you got the correct guess!")
                     elif guess < answer:
                         print("You guessed too low!")
                         guess_count -= 1
                     else:
                         print("You guessed too high!")
                         guess_count -= 1
-                        continue
                 else:
-                    print("I'm sorry, please enter a number between 1 and 9 to proceed")
-                    continue
+                    print("I'm sorry, please enter a number between 1 and {0} to proceed".format(highest))
 # 'except' handles a ValueError situation and loops back to the input
             except ValueError:
-                print("I'm sorry, please enter a number between 1 and 9 to proceed")
-                continue
+                print("I'm sorry, please enter a number between 1 and {0} to proceed".format(highest))
 
     print("GAME OVER")
 
