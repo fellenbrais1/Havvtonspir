aec039340e68f4c492e3c29eff414e154e3fdb6a
fellenbrais1 <106751977+fellenbrais1@users.noreply.github.com>
2024-01-29 01:42:30 +0900

    New uploads and fixes

--- Adventure.py
+++ Adventure.py
@@ -5,12 +5,15 @@
 available_exits = ['north', 'south', 'east', 'west']
 chosen_exit = ""
 while chosen_exit not in available_exits:
+    for item in available_exits:
+        print(item)
     chosen_exit = input('Please choose a direction: ')
     chosen_exit.casefold()
     if chosen_exit == 'quit':
         print('Game over!')
-        exit()
-print('Aren\'t you glad you got out of there!')
+        break
+else:
+    print('Aren\'t you glad you got out of there!')
 
 # While loops are useful when you don't know how many times the loop will have to execute in advance
 # For loops are useful when you know in advance how many times a loop will have to execute
---
+++ hi_lo_2.py
@@ -0,0 +1,80 @@
+
+# This code is a test of using the binary search process in a program
+# This version allows the program to say what the correct guess is when it closes in on it
+
+
+def hi_lo():
+
+    low = 1
+    high = 1000
+    guess_count = 0
+
+    print("Please think of a number between {0} and {1}".format(low, high))
+    input("Press enter to start...")
+
+    while low != high:
+        # The below formula is used to perform a binary search
+        # It will find the midpoint based off the low and high values
+        # Depending on if the guess is too high or low, it will find a new mid-point next time
+        # A computer can always guess any number between 1 and 1023 in 10 guesses
+        guess = low + (high - low) // 2
+        guess_count += 1
+        if guess_count == 0:
+            print("\nMy first guess will be {0}".format(guess))
+        elif guess_count == 10:
+            print("\nMy final guess will be {0}".format(guess))
+        elif guess_count == 11:
+            print("\nI have run out of guesses and lost the game!")
+            print("Well done human, you have beaten me!")
+            exit()
+        else:
+            print("\nMy next guess will be {0}".format(guess))
+        if guess_count == 1:
+            print("I have used {0} guess so far".format(guess_count))
+        else:
+            print("I have used {0} guesses so far".format(guess_count))
+        print("\nIs that correct, too high or too low?")
+        print("Type: 'c' for correct, 'h' for too high and 'l' for to low")
+        guess_loop = True
+        while guess_loop:
+            user_input = input("Your answer: ")
+            user_input.casefold()
+            if user_input == 'c':
+                if guess_count == 1:
+                    print("Ah! So the answer IS {0}!".format(guess))
+                    print("\nI got the answer correct on my first guess!")
+                    exit()
+                elif guess_count == 10:
+                    print("Ah! So the answer IS {0}!".format(guess))
+                    print("\nThat was lucky, I got it right on my last guess!")
+                    exit()
+                else:
+                    print("Ah! So the answer IS {0}!".format(guess))
+                    print("\nI got the correct answer in {0} guesses".format(guess_count))
+                    exit()
+            elif user_input == 'h':
+                print("\nOkay, I will guess lower next time...")
+                high = guess - 1
+                guess_loop = False
+            elif user_input == 'l':
+                print("\nOkay, I will guess higher next time...")
+                low = guess + 1
+                guess_loop = False
+            else:
+                print("\nI am sorry, please type 'c' for correct, 'h' for too high and 'l' for too low.")
+    else:
+        if guess_count == 1:
+            print("Ah! The answer is {0}!".format(high))
+            print("\nI got the answer correct on my first guess!")
+            exit()
+        elif guess_count == 10:
+            print("Ah! The answer is {0}!".format(high))
+            print("\nThat was lucky, I got it right on my last guess!")
+            exit()
+        else:
+            print("Ah! The answer is {0}!".format(high))
+            print("\nI got the correct answer in {0} guesses".format(guess_count))
+            exit()
+
+
+hi_lo()
