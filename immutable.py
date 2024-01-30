2965d4bc3f0f13c5b4731d97c98072917bc52910
fellenbrais1 <106751977+fellenbrais1@users.noreply.github.com>
2024-01-30 13:03:31 +0900

    New uploads
    New uploads:
    'immutable.py'
    'lists_intro.py'
    'mutable.py'

---
+++ immutable.py
@@ -0,0 +1,33 @@
+
+# Testing the 'id()' method to generate IDs for immutable objects
+
+# In this example, both variables have the same ID as they both evaluate to 'True'
+result = True
+another_result = result
+print(id(result))
+print(id(another_result))
+
+# Now, one will be different from the other, as 'result' is rebound to 'False'
+# Boolean values are an immutable data type
+result = False
+print()
+print(id(result))
+print(id(another_result))
+
+# The same thing but with a string, both IDs are the same
+result = "Correct"
+another_result = result
+print()
+print(id(result))
+print(id(another_result))
+
+# After reassignment to 'correct' for 'result' they will have different IDs
+result = 'correct'
+print()
+print(id(result))
+
+# Changing a string in any way will result in a new ID being created
+result += 'ish'
+print()
+print(id(result))
+print(id(another_result))
---
+++ mutable.py
@@ -0,0 +1,23 @@
+
+# Experimenting with mutable objects
+shopping_list = ["eggs", "cheese", "crackers", "milk", "meat"]
+
+# After assignment, we can see that both variable's IDs are the same
+another_list = shopping_list
+print(id(shopping_list))
+print(id(another_list))
+
+# We can see that even after changing a variable it keeps the same ID
+shopping_list.append("biscuits")
+
+# This means that both values continue to have the same ID despite having changed one of them
+print()
+print(id(shopping_list))
+print(id(another_list))
+
+# Experimenting with augmented assignment items to a list
+shopping_list += ["ammunition for my AR-15, yee-haw!"]
+print()
+print(shopping_list)
+print(id(shopping_list))
+print(id(another_list))
