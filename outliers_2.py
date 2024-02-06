c989019ffda9948974dca53c0c8c3499c04155ca
fellenbrais1 <106751977+fellenbrais1@users.noreply.github.com>
2024-02-06 23:56:40 +0900

    Updated code
    Updated 'outliers_2.py' to test out two
    new methods of removing multiple items
    from a list, the first is much more efficient in this case

--- outliers_2.py
+++ outliers_2.py
@@ -3,30 +3,54 @@
 # start when a value doesn't need to be deleted, and then stopping when /
 # reading from the end when an item doesn't need to be deleted
 
-# TODO Build this code to do what it says above
+# The code does this by running from the bottom until an item doesn't need to /
+# be deleted, then reversing the list and running from the top until an item /
+# doesn't need to be deleted, the list is then re-reversed to normal
 
-# min_valid = 100
-# max_valid = 200
-#
-# data = [4, 5, 104, 105, 110, 120, 130, 130, 150,
-#         160, 170, 183, 185, 187, 188, 191, 350, 360]
-#
-# deleting = True
-# while deleting:
-#     for index, value in enumerate(data):
-#         if (value < min_valid) or (value > max_valid):
-#             del data[index]
-#             break
-#     else:
-#         deleting = False
-# print("Deleting finished...")
-# print(data)
+# Overall, this results in a lot less loops than the other methods (in this /
+# use case)
+
+min_valid = 100
+max_valid = 200
+
+data = [4, 5, 104, 105, 110, 120, 130, 130, 150,
+        160, 170, 183, 185, 187, 188, 191, 350, 360]
+
+deleting = True
+while deleting:
+    for index, value in enumerate(data):
+        if value <= min_valid:
+            del data[index]
+            break
+        else:
+            deleting = False
+            break
+
+data = list(reversed(data))
+deleting_2 = True
+
+while deleting_2:
+    for index, value in enumerate(data):
+        if value <= value >= max_valid:
+            del data[index]
+            break
+        else:
+            deleting_2 = False
+            break
+
+data = list(reversed(data))
+
+print("\nDeleting finished...")
+print("Cleansed data:", data)
+
+# -----------------------------------------------------------------------------
 
 # Also, another list of deleted items could be created when scanning the list /
 # then these items can be deleted from the list all in one go
 
-# TODO Make this code as well, need a function to delete multiple items from /
-# TODO a list object
+# This code is not really much more efficient than the code above, it still /
+# has to constantly re-loop around the code to get a result, and it also has /
+# to do another for loop to generate the 'items_to_delete' list
 
 min_valid = 100
 max_valid = 200
@@ -37,16 +61,25 @@ data = [4, 5, 104, 105, 110, 120, 130, 130, 150,
 list_to_delete = []
 
 deleting = True
+cleansing = True
+
 while deleting:
     for index, value in enumerate(data):
-        if (value < min_valid) or (value > max_valid):
+        if (value <= min_valid) or (value >= max_valid):
             list_to_delete.append(data[index])
     else:
-        for item in data:
-            if item in list_to_delete:
-                del data(item)
-        deleting = False
-print("Deleting finished...")
-print(data)
+        while cleansing:
+            for item in data:
+                if item in list_to_delete:
+                    data.remove(item)
+                    break
+                else:
+                    continue
+            else:
+                deleting = False
+                cleansing = False
 
-print(list_to_delete)
+print("\nDeleting finished...")
+
+print("Cleansed data:", data)
+print("Elements deleted:", list_to_delete)
