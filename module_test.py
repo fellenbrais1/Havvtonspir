89fe99124d7bcdaacfd4642f457f83b3d6245354
fellenbrais1 <106751977+fellenbrais1@users.noreply.github.com>
2024-02-22 23:57:29 +0900

    New uploads and upgrades
    More experiments with referencing data in dictionaries and creating functions

--- module_test.py
+++ module_test.py
@@ -1,24 +1,39 @@
 
 from data_test import battlers, battlers_data
 
-for item in battlers_data['c_1']['statuses']:
-    print(item)
-print(battlers_data['c_1']['statuses'])
-for item in battlers_data['c_2']['statuses']:
-    print(item)
-for item in battlers_data['c_3']['statuses']:
-    print(item)
-for item in battlers_data['e_1']['statuses']:
-    print(item)
-print(battlers_data)
+
+def print_stats():
+    a = b = c = d = []
+    for item in battlers_data['c_1']['statuses']:
+        a += [item]
+        # print(item, sep=', ', end='\n',)
+    for item in battlers_data['c_2']['statuses']:
+        b += [item]
+        # print(item, sep=', ', end='\n')
+    for item in battlers_data['c_3']['statuses']:
+        c += [item]
+        # print(item, sep=', ', end='\n')
+    for item in battlers_data['e_1']['statuses']:
+        d += [item]
+        # print(item, sep=', ', end='\n')
+    print(a, b, c, d)
+    print(battlers_data)
 
 
-def init():
-    for name in battlers:
-        ref = name
-        ref['init_mod'] \
-            = ref['speed']
-        print(ref['name'], ref['init_mod'])
+def init(active_name):
+    r = active_name
+    r['init_mod'] \
+        = r['speed']
+    print(r['name'], r['init_mod'])
 
 
-init()
+# for battler in battlers:
+#     active_name = battler
+#     init(active_name)
+
+print_stats()
+
+
+if __name__ == 'main':
+    print_stats()
+    init(active_name=None)
---
+++ module_test_2.py
@@ -0,0 +1,7 @@
+
+from data_test import battlers
+from module_test import init
+
+for battler in battlers:
+    active_name = battler
+    init(active_name)
